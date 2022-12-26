0941884d4b8b62ac78dfe2364829170b54f3b6b1
Aju Ani Justus <35808017+ajuanijustus@users.noreply.github.com>
2022-12-26 01:39:51 +0000

    Time: 104 ms (83.08%), Space: 13.9 MB (34.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -10,30 +10,30 @@ class Solution:
                     continue
                 else:
                     x = int(x)
-                    
+                
                 if i in row.keys():
-                    row[i].append(x)
+                    if x in row[i]:
+                        return False
+                    else:
+                        row[i].append(x)
                 else:
                     row[i] = [x]
                     
                 if j in col.keys():
-                    col[j].append(x)
+                    if x in col[j]:
+                        return False
+                    else:
+                        col[j].append(x)
                 else:
                     col[j] = [x]
                 
                 box_id = str(i//3) + "_" + str(j//3)
                 if box_id in box.keys():
-                    box[box_id].append(x)
+                    if x in box[box_id]:
+                        return False
+                    else:
+                        box[box_id].append(x)
                 else:
                     box[box_id] = [x]
-        
-        for r in row.values():
-            if len(r) != len(set(r)):
-                return False
-        for c in col.values():
-            if len(c) != len(set(c)):
-                return False
-        for b in box.values():
-            if len(b) != len(set(b)):
-                return False
+
         return True
